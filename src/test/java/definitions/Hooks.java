8fa4eb0a9a415ab925c0d9163f1ed4245cac14eb
Joel <joel.leiva@icloud.com>
2024-01-10 01:45:00 -0500

    estructura de appium y pom

---
+++ src/test/java/definitions/Hooks.java
@@ -0,0 +1,12 @@
+package definitions;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class Hooks {
+    //variable publica que se pueda ver cualqueir lugar
+    public static AndroidDriver driver;
+
+
+
+
+}
